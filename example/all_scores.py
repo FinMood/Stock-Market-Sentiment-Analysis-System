0a5074aefc21e3e55b42a5623d6bfd23d9e43870
Cuei-Sian <doreen20080615@gmail.com>
2026-08-15 15:16:58 +0800

    更新all_scores.py及整合五種情緒分析的CSV檔

--- example/all_scores.py
+++ example/all_scores.py
@@ -1,24 +1,27 @@
 import pandas as pd
 
-# --- 整合四個模型的分數 ---
+# --- 整合五個模型的分數 ---
 # 1. 讀取原始新聞資料檔
 df_final = pd.read_csv("source/TaiwanStockNews_test.csv")
 
-# 2. 依序讀取四個模型的 CSV 並將最後一欄 score 提取出來寫入
+# 2. 依序讀取五個模型的 CSV 並將最後一欄 score 提取出來寫入
 df_Roberta = pd.read_csv("score_data/news_sentiment_wang.csv")
 df_ckipbert = pd.read_csv("score_data/TaiwanStockNews_test_w_ckipbert_scores_1.csv")
 df_finbert = pd.read_csv(
     "score_data/TaiwanStockNews_test_w_finbert_scores_1.csv"
 )
 df_jieba = pd.read_csv("score_data/TaiwanStockNews_test_w_jieba_scores_1.csv")
+df_llm = pd.read_csv("score_data/llm_results.csv")
 
-# 3. 新增四個對應的模型分數欄位
+# 3. 新增五個對應的模型分數欄位
 # 翠賢模型
 df_final["score_Roberta"] = df_Roberta["score_normalized"]
 # 至得模型
 df_final["score_ckipbert"] = df_ckipbert["score"]
 df_final["score_finbert"] = df_finbert["score"]
 df_final["score_jieba"] = df_jieba["score"]
+#JOY模型(LLM)
+df_final["score_llm"] = df_llm["llm_score"]
 
 # 4. 寫入並儲存至 score_data 資料夾
 df_final.to_csv(
@@ -27,4 +30,4 @@ df_final.to_csv(
     encoding="utf-8-sig",
 )
 
-print("🎉 完美整合！四個模型的分數已成功寫入 TaiwanStockNews_test_all_scores.csv")
+print("🎉 完美整合！五個模型的分數已成功寫入 TaiwanStockNews_test_all_scores.csv")
